9ead441ecd54aa39fa8b1ead5332c80a9b37d08a
No_Star <105438922+SVFWeb@users.noreply.github.com>
2025-12-09 11:39:36 +0800

    重大更新！！！
    对项目文件结构进行优化

--- campus_activity/unpackage/dist/dev/mp-weixin/common/vendor.js
+++ campus_activity/unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7290,7 +7290,7 @@ function isConsoleWritable() {
 function initRuntimeSocketService() {
   const hosts = "26.245.181.160,192.168.1.106,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_rqcAPg";
+  const id = "mp-weixin_UlKPmp";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
@@ -8716,7 +8716,7 @@ class S {
 function T(e2) {
   return e2 && "string" == typeof e2 ? JSON.parse(e2) : e2;
 }
-const b = true, E = "mp-weixin", A = T(define_process_env_UNI_SECURE_NETWORK_CONFIG_default), P = E, C = T('{"address":["127.0.0.1","26.245.181.160","192.168.1.106"],"servePort":7001,"debugPort":9001,"initialLaunchType":"local","skipFiles":["<node_internals>/**","D:/HBuilderX/plugins/unicloud/**/*.js"]}'), O = T('[{"provider":"alipay","spaceName":"ljy-data","spaceId":"env-00jxuc2o76k9","spaceAppId":"2021006114668726","accessKey":"74li80axEPh8JZpK","secretKey":"SIsUTTWCmLGEKJnL"}]') || [];
+const b = true, E = "mp-weixin", A = T(define_process_env_UNI_SECURE_NETWORK_CONFIG_default), P = E, C = T('{"address":["127.0.0.1","26.245.181.160","192.168.1.106"],"servePort":7000,"debugPort":9000,"initialLaunchType":"local","skipFiles":["<node_internals>/**","D:/HBuilderX/plugins/unicloud/**/*.js"]}'), O = T('[{"provider":"alipay","spaceName":"ljy-data","spaceId":"env-00jxuc2o76k9","spaceAppId":"2021006114668726","accessKey":"74li80axEPh8JZpK","secretKey":"SIsUTTWCmLGEKJnL"}]') || [];
 let N = "";
 try {
   N = "__UNI__D37EE2B";
